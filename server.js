a161c5e0bb3228fcd18d803eee8f5fc56b12b4aa
niranjan <niranjan64461231@gmail.com>
2025-11-05 21:32:40 +0530

    c

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
